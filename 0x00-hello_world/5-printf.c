f4829add527ea371e3d94891a8dac1f98d47cc4b
liloodz <lily86@gmail.com>
2020-09-16 22:47:55 +0000

    My commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-char string[] = "with proper grammar, but the outcome is a piece of art\,";
-printf(string);
+printf("with proper grammar, but the outcome is a piece of art\,");
 return (0);
 }
