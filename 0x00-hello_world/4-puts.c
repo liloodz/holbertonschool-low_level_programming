85c1a2c2bee5fd29abdd27f1452082f1907e3d9b
liloodz <lily86@gmail.com>
2020-09-16 22:05:14 +0000

    My commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+* main - Prints a string
+* Return: 0
+*/
+
+
+int main(void)
+{
+char string [] = "\"Programming is like building a multilingual puzzle"\n
+return (0);
+}
