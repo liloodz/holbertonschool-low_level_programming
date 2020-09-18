197a078f4c84855d638df11926c6f6920b9f01b6
liloodz <lily86@gmail.com>
2020-09-18 02:19:31 +0000

    My commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,33 +1,26 @@
 #include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
 
 /**
- * main - Prints all the numbers of base 16 in lowercase, followed by a new line.
+ * main - Prints all the numbers of base 16 in lowercase
  * Return: 0
  */
-
 
 int main(void)
 {
 int n;
 
 n = 48;
-
-while (n < 59)
+while (n < 58)
 {
 putchar(n);
 n++;
 }
-
 n = 97;
-
 while (n < 103)
 {
 putchar(n);
 n++;
 }
-
 putchar('\n');
 return (0);
 }
